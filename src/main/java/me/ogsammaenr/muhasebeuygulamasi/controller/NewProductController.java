534891f5395b066f170043891d9368ea97a00776
ogsammaenr <osmangamer152@gmail.com>
2025-06-18 00:35:38 +0300

    add new fxml variables

--- src/main/java/me/ogsammaenr/muhasebeuygulamasi/controller/NewProductController.java
+++ src/main/java/me/ogsammaenr/muhasebeuygulamasi/controller/NewProductController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import me.ogsammaenr.muhasebeuygulamasi.util.Utils;
@@ -74,4 +76,86 @@ public class NewProductController {
     private AnchorPane parentPane;
     @FXML
     private Button next;
+
+    /*=====================================*/
+
+    @FXML
+    private TextField txt_PVCFiyat;
+    @FXML
+    private TextField txt_PVCRenk;
+    @FXML
+    private TextField txt_PVCFirma;
+    @FXML
+    private TextField txt_dolar;
+    @FXML
+    private Label lbl_hammadde;
+    @FXML
+    private Label lbl_hammaddeAlan;
+    @FXML
+    private Label lbl_kesimFiyatBirim;
+    @FXML
+    private TextField txt_kesimFiyat;
+    @FXML
+    private Label lbl_CNCFiyatBirim;
+    @FXML
+    private TextField txt_CNCFiyat;
+    @FXML
+    private Label lbl_zimparaIscilikBirim;
+    @FXML
+    private TextField txt_zimparaIscilik;
+    @FXML
+    private Label lbl_zimparaFiyatBirim;
+    @FXML
+    private TextField txt_zimparaFiyat;
+    @FXML
+    private Label lbl_paletIscilikBirim;
+    @FXML
+    private TextField txt_paletIscilik;
+    @FXML
+    private Label lbl_tutkalFiyatBirim;
+    @FXML
+    private TextField txt_tutkalFiyat;
+    @FXML
+    private Label lbl_PVCBirim;
+    @FXML
+    private TextField txt_PVC;
+    @FXML
+    private Label lbl_basimIscilikBirim;
+    @FXML
+    private TextField txt_basimIscilik;
+    @FXML
+    private Label lbl_ambalajBirim;
+    @FXML
+    private TextField txt_ambalaj;
+    @FXML
+    private Label lbl_nakliyeBirim;
+    @FXML
+    private TextField txt_nakliye;
+    @FXML
+    private Label lbl_kalinlastirmaIscilikBirim;
+    @FXML
+    private TextField txt_kalinlastirmaIscilik;
+    @FXML
+    private Label lbl_karBirim;
+    @FXML
+    private TextField txt_karOran;
+    @FXML
+    private Label lbl_takimFiyat;
+    @FXML
+    private Label lbl_birimFiyat;
+    @FXML
+    private Label lbl_hammaddeFiyat;
+    @FXML
+    private Label lbl_hammaddeBirimFiyat;
+    @FXML
+    private Label lbl_iscilikFiyat;
+    @FXML
+    private Label lbl_iscilikBirimFiyat;
+    @FXML
+    private Label lbl_kar;
+    @FXML
+    private Label lbl_birimKar;
+    @FXML
+    private Button btn_save;
+
 }
