e4557fe4cc27ee9aa3bdee9ddcdb0c748daa5de1
ogsammaenr <osmangamer152@gmail.com>
2025-06-18 03:45:50 +0300

    add new controller class

--- src/main/java/me/ogsammaenr/muhasebeuygulamasi/controller/NewProductController.java
+++ src/main/java/me/ogsammaenr/muhasebeuygulamasi/controller/NewProductController.java
@@ -2,25 +2,30 @@ package me.ogsammaenr.muhasebeuygulamasi.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import me.ogsammaenr.muhasebeuygulamasi.util.Utils;
 import org.jetbrains.annotations.NotNull;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class NewProductController {
-    private Map<String, Group> units = new HashMap<>();
+    private Map<String, Group> units;
     private int unitCounter = 1;
 
 
     @FXML
     public void initialize() {
+        this.units = new HashMap<>();
+
         units.put("Unit-1", textGroup);
         parentPane.widthProperty().addListener((obs, oldVal, newVal) -> {
             double centerX = newVal.doubleValue() / 2;
@@ -56,8 +61,25 @@ public class NewProductController {
 
     @FXML
     public void onNextClick(ActionEvent event) {
+        try {
+            URL fxmlUrl = getClass().getResource("/me/ogsammaenr/muhasebeuygulamasi/new-product-view2.fxml");
+            System.out.println("FXML URL: " + fxmlUrl);
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
 
+            Parent newProductView = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Product Muhasebe Uygulamasi");
+            stage.setAlwaysOnTop(true);
+            stage.setMinWidth(900);
+            stage.setMinHeight(600);
+            stage.setScene(new Scene(newProductView));
+            stage.show();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     private void handleDeleteUnit(@NotNull Group unit) {
         units.remove(unit.getId());
@@ -76,86 +98,4 @@ public class NewProductController {
     private AnchorPane parentPane;
     @FXML
     private Button next;
-
-    /*=====================================*/
-
-    @FXML
-    private TextField txt_PVCFiyat;
-    @FXML
-    private TextField txt_PVCRenk;
-    @FXML
-    private TextField txt_PVCFirma;
-    @FXML
-    private TextField txt_dolar;
-    @FXML
-    private Label lbl_hammadde;
-    @FXML
-    private Label lbl_hammaddeAlan;
-    @FXML
-    private Label lbl_kesimFiyatBirim;
-    @FXML
-    private TextField txt_kesimFiyat;
-    @FXML
-    private Label lbl_CNCFiyatBirim;
-    @FXML
-    private TextField txt_CNCFiyat;
-    @FXML
-    private Label lbl_zimparaIscilikBirim;
-    @FXML
-    private TextField txt_zimparaIscilik;
-    @FXML
-    private Label lbl_zimparaFiyatBirim;
-    @FXML
-    private TextField txt_zimparaFiyat;
-    @FXML
-    private Label lbl_paletIscilikBirim;
-    @FXML
-    private TextField txt_paletIscilik;
-    @FXML
-    private Label lbl_tutkalFiyatBirim;
-    @FXML
-    private TextField txt_tutkalFiyat;
-    @FXML
-    private Label lbl_PVCBirim;
-    @FXML
-    private TextField txt_PVC;
-    @FXML
-    private Label lbl_basimIscilikBirim;
-    @FXML
-    private TextField txt_basimIscilik;
-    @FXML
-    private Label lbl_ambalajBirim;
-    @FXML
-    private TextField txt_ambalaj;
-    @FXML
-    private Label lbl_nakliyeBirim;
-    @FXML
-    private TextField txt_nakliye;
-    @FXML
-    private Label lbl_kalinlastirmaIscilikBirim;
-    @FXML
-    private TextField txt_kalinlastirmaIscilik;
-    @FXML
-    private Label lbl_karBirim;
-    @FXML
-    private TextField txt_karOran;
-    @FXML
-    private Label lbl_takimFiyat;
-    @FXML
-    private Label lbl_birimFiyat;
-    @FXML
-    private Label lbl_hammaddeFiyat;
-    @FXML
-    private Label lbl_hammaddeBirimFiyat;
-    @FXML
-    private Label lbl_iscilikFiyat;
-    @FXML
-    private Label lbl_iscilikBirimFiyat;
-    @FXML
-    private Label lbl_kar;
-    @FXML
-    private Label lbl_birimKar;
-    @FXML
-    private Button btn_save;
-
 }
