0de19b8c95258399c508f85c5370b40d52114f1a
ogsammaenr <osmangamer152@gmail.com>
2026-01-29 16:23:30 +0300

    başlangıç

---
+++ MuhasebeUygulamasiv3/src/main/java/me/ogsammaenr/muhasebeuygulamasiv3/controller/MainController.java
@@ -0,0 +1,4 @@
+package me.ogsammaenr.muhasebeuygulamasiv3.controller;
+
+public class MainController {
+}
---
+++ MuhasebeUygulamasiv3/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module me.ogsammaenr.muhasebeuygulamasiv3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens me.ogsammaenr.muhasebeuygulamasiv3 to javafx.fxml;
+    exports me.ogsammaenr.muhasebeuygulamasiv3;
+}
